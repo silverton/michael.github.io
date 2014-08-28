018efa35b7c3a87caefb6668869943c7c57fb8d7
michael <michael@silverton.palo-alto.ca.us>
2014-08-28 03:43:12 +0000

    Create index.2010.php

---
+++ index.2010.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>   
+<meta name="google-site-verification" content="TAVsqESmcNC180H4I9Jhli5rtnqsTWpI19Jt-cVKAQs" />
+   <meta name="alexaVerifyID" content="l40LvjZ_8nqz2-ysmvsq_HKrcVA" />
+   <title>Michael Silverton | Palo Alto, California</title>
+<!-- ***** ***** ***** ***** ***** -->
+<!-- REMEMBER TO REMOVE THESE IF YOU WANT THE PAGE INDEXED! -->
+<!-- ***** ***** ***** ***** ***** -->
+
+<META NAME="ROBOTS" content="NOINDEX, NOFOLLOW" />
+<META NAME="ROBOTS" content="NOARCHIVE" />
+<META NAME="GOOGLEBOT" CONTENT="NOINDEX, NOFOLLOW" />
+<META NAME="GOOGLEBOT" CONTENT="NOARCHIVE" />
+<META NAME="GOOGLEBOT" CONTENT="NOSNIPPET" />
+<META NAME="GOOGLEBOT" CONTENT="NOODP" />
+<META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE" />
+<META HTTP-EQUIV="PRAGMA" CONTENT="NO-CACHE" />
+
+<!-- ***** ***** ***** ***** ***** -->
+<!-- REMEMBER TO REMOVE ABOVE IF YOU WANT THE PAGE INDEXED! -->
+<!-- ***** ***** ***** ***** ***** -->
+
+   <script src="http://platform.twitter.com/anywhere.js?id=4EiliLPGaX0vqypWPUCkWw&amp;v=1" type="text/javascript">
+     </script>
+   <meta name="keywords" content="Michael Silverton, Michael Silverton, Michael Silverton, Michael Silverton, Michael Silverton">
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+   <link rel="stylesheet" type="text/css" href="uac.css" />
+   <link rel="me" href="http://michael.silverton.palo-alto.ca.us" />
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-3293921-4']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+   </head>
+   <body>
+<script type="text/javascript">var _sf_startpt=(new Date()).getTime()</script>
+   <div id="header">
+   <!-- Include the Google Friend Connect javascript library. --> <script type="text/javascript" src="http://www.google.com/friendconnect/script/friendconnect.js"></script> <!-- Define the div tag where the gadget will be inserted. --> <div id="div-5619941974682203707"></div> <!-- Render the gadget into a div. --> <script type="text/javascript"> var skin = {}; skin['BORDER_COLOR'] = '#cccccc'; skin['ENDCAP_BG_COLOR'] = '#e0ecff'; skin['ENDCAP_TEXT_COLOR'] = '#333333'; skin['ENDCAP_LINK_COLOR'] = '#0000cc'; skin['ALTERNATE_BG_COLOR'] = '#ffffff'; skin['CONTENT_BG_COLOR'] = '#ffffff'; skin['CONTENT_LINK_COLOR'] = '#0000cc'; skin['CONTENT_TEXT_COLOR'] = '#333333'; skin['CONTENT_SECONDARY_LINK_COLOR'] = '#7777cc'; skin['CONTENT_SECONDARY_TEXT_COLOR'] = '#666666'; skin['CONTENT_HEADLINE_COLOR'] = '#333333'; skin['POSITION'] = 'top'; skin['DEFAULT_COMMENT_TEXT'] = '\x3d\x3d\x3e enter your site-tweet here \x3c\x3d\x3d'; skin['HEADER_TEXT'] = 'Comments'; google.friendconnect.container.setParentUrl('/' /* location of rpc_relay.html and canvas.html */); google.friendconnect.container.renderSocialBar( { id: 'div-5619941974682203707', site: '15248214227973996831', 'view-params':{"scope":"SITE","features":"video,comment","showWall":"true"} }, skin); </script>
+   <p>&nbsp;</p><br />
+<table align="center" width="780>
+<tr valign="middle"><td align="left"><a href="http://past.silverton.palo-alto.ca.us/">< < < past</a></td><td align="center"><a href="http://activitystream.silverton.palo-alto.ca.us/">< < < present > > ></a></td><td align="right"><a href="http://future.silve
+rton.palo-alto.ca.us/">future > > ></a></td></tr>
+</table>
+   </div>
+   <div id="main_container">
+   <table width="780" border="0">
+<tr><td colspan="2" align="center">
+<!-- 
+<hr />
+<a href="http://gan.doubleclick.net/gan_click?lid=41000000027728643&pubid=21000000000304017" target="_blank">70% off Great Courses DVDs</a> | <a href="http://gan.doubleclick.net/gan_click?lid=41000000033026982&pubid=21000000000304017" target="_blank">Halloween Flowers</a> | <a href="http://gan.doubleclick.net/gan_click?lid=41000000032717350&pubid=21000000000304017" target="_blank">Kitchen Herb Garden</a> | <a href="http://gan.doubleclick.net/gan_click?lid=41000000033017858&pubid=21000000000304017" target="_blank">$25 off Great Courses materials</a>
+<hr />
+-->
+<hr />
+<div align="center">In ancient times, personal web pages used to look like this ...</div>
+<hr />
+<font color="white"><h2><a href="http://michael.silverton.palo-alto.ca.us" target="_top">Michael.Silverton.Palo-Alto.CA.us</a></h2>
+"Changes aren't permanent, but change is<a href="http://flavors.me/silverton" target="_blank">.</a>" <a href="http://michael.silverton.palo-alto.ca.us/" rel="me"></a>- Neil Peart<br /><br />
+<!-- 
+If you just need a quick <a href="http://j.mp/whoismichaelsilverton" target="_blank">whois</a/>.<br /><br />
+-->
+<!-- Now YOU can produce Local Investigative Journalism.<br />Help develop the market for high quality citizen-sourced journalism at <a href="http://spot.us" target="_blank">Spot.Us</a>.<br /><br />
+<script type='text/javascript'>
+var spotus_lite_host = 'http://silverton.palo-alto.ca.us/'; 
+// Example: If you can access the helper.html file at http://www.domain.com/path/helper.html, then you add http://www.domain.com/path there.
+var spotus_lite_url = 'http://spot.us/lite/480-has-arizona-s-sb-1070-revealed-a-political-wedge-between-the-latino-and-lgbt-communities';
+</script> <script src='http://spot.us/javascripts/spotus_lite_min.js' type='text/javascript'></script> <iframe width='300' src='' border='1' onChange='resizeIframe();' onLoad='resizeIframe();' id='spotus_lite' scrolling='no' noscroll></iframe> -->
+<!-- Update message 
+<h3 align="left">10/7/2010 1800PDT Heads Up: I've begun migration of this domain's email service to Apps for Domains. If I fail to respond to your email promptly, please know that I will respond just as quickly as the migration allows. As an urgent backup, you can reach me on <a href="http://twitter.com/silverton">Twitter</a> or <a href="http://profiles.google.com/michael.silverton">Google Profiles</a>. Thanks for the patience and understanding!</h3>
+-->
+Fast Company: Nov 2010 <a href="http://fastcompany.com/influence" target="_blank">Influence Project</a>.<br />Percentage of participants you ranked ahead of: <a href="http://fcinf.com/v/ad60" target="_blank">93%</a>
+<br /><br />
+Open Activity Streams: Follow Me <a href="http://silverton.palo-alto.ca.us/michael">@michael@silverton.palo-alto.ca.us</a>
+<br />
+If the <a href="http://www.screentoaster.com/watch/stUE9URU1BRFtXR11VXFxcV1NQ/intro_to_adaptive_futuretechture" target="_blank">Screentoaster version</a> with voice-over embedded below has issues; try backup <a href="https://prezi.com/secure/d1b951b388321a058f5b90f1e767738506875d40/">link to original Prezi</a>.<br />
+<object id='stUE9URU1BRFtXR11VXFxcV1NQ' width='600' height='485' type='application/x-shockwave-flash' data='http://www.screentoaster.com/swf/STPlayer.swf'  codebase='http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,115,0'><param name='movie' value='http://www.screentoaster.com/swf/STPlayer.swf'/><param name='allowFullScreen' value='true'/><param name='allowScriptAccess' value='always'/><param name='flashvars' value='video=stUE9URU1BRFtXR11VXFxcV1NQ'/></object><div style='width: 600px; text-align: right;'><a href='http://www.screentoaster.com/'>Free online screencasting tool</a></div>
+<br />
+<div align="center"><h3>Substrate Independence<a href="http://metavalent.com/?p=1340" target="_blank">.</a> Forget the Network<a href="http://metavalent.com/?p=1340" target="_blank">.</a> You are the Computer<a href="http://metavalent.com/?p=1340" target="_blank">.</a></h3></div>
+"Just give me <a href="http://j.mp/bandwidthandabrowser" target="_blank">bandwidth and a browser</a>." (1997) | "RIP: The <a href="http://j.mp/desktopisdead" target="_blank">Desktop is Dead</a>." (2007)
+<br /><br />
+<script src="http://curated.by/embed/embed.js"></script>
+<script>
+_bundleWidget({
+	bundle_id:1635,
+	title:'adaptive futuretechture',
+	subtitle:'curated.by/silverton',
+	scrolling:true,
+	update:true,
+	shell:true,
+	auto_width:false,
+	per_page:20,
+	font_size:'11px',
+	border_color:'666666',
+	heading_text_color:'ffffff',
+	heading_bg_color:'666666',
+	link_color:'666666',
+	text_color:'333333',
+	content_bg_color:'ffffff',
+	content_divider_color:'cccccc',
+	width:600,
+	height:400
+});
+</script>
+<br /><br />
+<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,115,0" width="600" height="450" id="qikPlayer" align="middle"><param name="allowScriptAccess" value="sameDomain" /><param name="allowFullScreen" value="true" /><param name="movie" value="http://qik.com/swfs/qikPlayer5.swf" /><param name="quality" value="high" /><param name="bgcolor" value="#333333" /><param name="FlashVars" value="streamID=8aee49fdf13341daa71d1421c6f7a9c4&amp;autoplay=false" /><embed src="http://qik.com/swfs/qikPlayer5.swf" quality="high" bgcolor="#333333" width="600" height="450" name="qikPlayer" align="middle" allowScriptAccess="sameDomain" allowFullScreen="true" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" FlashVars="streamID=8aee49fdf13341daa71d1421c6f7a9c4&amp;autoplay=false"></embed></object>
+<br />
+This video is actually only 4:35, at which point the QIK app locked up and wouldn't close.<br />Subsequent QIK versions are performing much better!
+
+<script src="http://cdn.wibiya.com/Toolbars/dir_0602/Toolbar_602139/Loader_602139.js" type="text/javascript"></script>
+<!-- FOOTER -->
+<?php include("footer.index.php"); ?>
+<script src="http://tweetboard.com/tb.js?v=1.0&user=silverton" type="text/javascript"></script>
+</body>
+</html>
