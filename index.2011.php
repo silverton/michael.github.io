08fe16ff0bc634530cc5f6d6a8cd337a5a0b3bbf
michael <michael@silverton.palo-alto.ca.us>
2014-08-28 03:43:52 +0000

    Create index.2011.php

---
+++ index.2011.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en" itemscope itemtype="http://schema.org/Person">
+<head>
+   <title>Michael Silverton | Palo Alto, California</title>
+   <script src="http://platform.twitter.com/anywhere.js?id=4EiliLPGaX0vqypWPUCkWw&amp;v=1" type="text/javascript">
+     </script>
+   <META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE" />
+   <META HTTP-EQUIV="PRAGMA" CONTENT="NO-CACHE" />
+   <meta name="description" content="Michael Silverton, Palo Alto, California" />
+   <meta name="keywords" content="Michael Silverton, Michael Silverton, Michael Silverton, Michael Silverton, Michael Silverton">
+   <meta http-equiv="Expires" content="Fri, 30 Dec 2011 12:00:00 GMT" />
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+   <link rel="stylesheet" type="text/css" href="home.css" />
+   <link rel="me" href="http://michael.silverton.palo-alto.ca.us" />
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-3293921-4']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+   </head>
+   <body>
+<script type="text/javascript">var _sf_startpt=(new Date()).getTime()</script>
+
+<!-- admob -->
+<script type="text/javascript">
+var admob_vars = {
+ pubid: 'a14e30294e6399c', // publisher id
+ bgcolor: '000000', // background color (hex)
+ text: 'FFFFFF', // font-color (hex)
+ test: true // test mode, set to false to receive live ads
+};
+</script>
+<script type="text/javascript" src="http://mmv.admob.com/static/iphone/iadmob.js"></script>
+<!-- end admob -->
+<div id="main">
+<table width="720" border="0">
+<tr><td colspan="2" align="center">
+<font color="white"><h2>Michael.Silverton.Palo-Alto.CA.us</h2>
+<h3><a href="http://buy.silverton.palo-alto.ca.us/" target="_top">buy</a> | <a href="http://contact.silverton.palo-alto.ca.us/" target="_top">contact</a> | <a href="http://silverton.palo-alto.ca.us/" target="_top">status</a> | <a href="http://activitystream.silverton.palo-alto.ca.us" target="_top">stream</a> | <a href="http://whois.silverton.palo-alto.ca.us/" target="_top">whois</a><a href="http://michael.silverton.palo-alto.ca.us/" rel="me"></a></h3>
+<h3>Innovation Market Research &middot; Technology Foresight &middot; Business Strategy</h3><hr />
+<p style="text-align:center; color:gray">What is it to login to a human? On the <a href="http://GlobalCognitionGroup.com/" target="_new">global cognition grid</a>, <a href="http://j.mp/ToDreamTheImpossibleStream">you will</a>.</p>
+<!-- custom search -->
+<form action="http://Michael.Silverton.Palo-Alto.CA.us/searchresults.php" id="cse-search-box">
+  <div>
+    <input type="hidden" name="cx" value="partner-pub-1640665942302807:2806235463" />
+    <input type="hidden" name="cof" value="FORID:10" />
+    <input type="hidden" name="ie" value="UTF-8" />
+    <input type="text" name="q" size="55" />
+  </div>
+</form>
+<script type="text/javascript" src="http://www.google.com/cse/brand?form=cse-search-box&lang=en"></script>
+<!-- //end custom search -->
+<h3>Themes &middot; Memes</h3>
+<p><a href="http://j.mp/whatISopenscience" target="_blank" title="... and other dropped Packets of Consciousness over time.">Open Science</a> | <a href="http://michael.silverton.palo-alto.ca.us/packets/1828/" target="_blank" title="Please reload the prezi a couple of times if the cloud server is acting silly. Sorry for the inconvenience.">Adaptive Futuretechture</a> | <a href="http://EthernetTV.net/" target="_blank" title="Curiously curated content since 1999.">EthernetTV</a> | <a href="http://ethernettv.net/archives/498" target="_blank" title="'The future of the future is the present.'">Post-Future Present Tense</a> | <a href="https://en.wikipedia.org/wiki/Cornucopian" target="_blank">Refractory Cornucopian</a></p>
+<p><a href="http://temporaryhuman.com/" target="_blank" title="Launched with t-minus 17 years on the DNA time clock.">Temporary Human</a> | <a href="http://ApprehendingPostscarcity.blogspot.com" target="_blank" title="It's time to boot up the star trek economy. Not in the future. Right Now.">Apprehending Postscarcity</a> | <a href="http://metavalent.com/" target="_blank" title="Where do novel ideas come from and how do they percolate planet-wide to become accepted as default reality?">Substrate Independence</a> | <a href="http://trendytropes.tumblr.com/" target="_blank" title="Hear today, repeat tomorrow, gone a day later.">Trendy Tropes</a> | <a href="http://wtf.silverton.palo-alto.ca.us/" target="_blank">WTF</a></p>
+<br />
+<div style="text-align: center; color: gray;">Bitcoin accepted:1M5SQs2yEqK2rRY5WNu5LvYFAHCxUCNsHP</div>
+</div>
+
+<!-- FOOTER -->
+<?php include("footer.index.php"); ?>
+
+<!-- adsense mobile -->
+<script type="text/javascript"><!--
+  // XHTML should not attempt to parse these strings, declare them CDATA.
+  /* <![CDATA[ */
+  window.googleAfmcRequest = {
+    client: 'ca-mb-pub-1640665942302807',
+    format: '320x50_mb',
+    output: 'html',
+    slotname: '3960828520',
+  };
+  /* ]]> */
+//--></script>
+<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_afmc_ads.js"></script>
+<!-- end adsense mobile -->
+
+</body>
+</html>
